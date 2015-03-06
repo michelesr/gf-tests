f542fcaf661ba6d9157b1dcffb137109187ab1cf
MicheleSr <mikefender@cryptolab.net>
2015-03-06 12:35:36 +0100

    add createuser on db script

--- db/reset_db.sh
+++ db/reset_db.sh
@@ -4,5 +4,6 @@
 # Use also to import db the first time.
 
 dropdb -U postgres gf_dev
+createuser -U postgres -D -A gf_dev
 createdb -U postgres -O gf_dev -E utf-8 -T template0 gf_dev
 psql -U postgres -d gf_dev < db_test.sql
